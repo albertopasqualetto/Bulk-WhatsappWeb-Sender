8d3597a170fe42de6d175d15eb646894552a8436
Alberto Pasqualetto <39854348+albertopasqualetto@users.noreply.github.com>
2022-04-29 18:08:07 +0200

    Whatsapp is now waiting
    Refactor

---
+++ WASend.js
@@ -0,0 +1,82 @@
+module.exports.send = SendMessages
+
+const { Client, NoAuth, MessageMedia } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+const path = require('path');
+const fs = require('fs');
+
+//Whatsapp magic
+function SendMessages(numbersFile, messageToSend, mediaToSend){
+    const client = new Client({
+        authStrategy: new NoAuth(),
+        puppeteer: {
+            headless: true,
+            //executablePath: './.local-chromium/win64-982053/chrome-win/chrome.exe'      //for pkg output win
+            //executablePath: './.local-chromium/linux-982053/chrome-linux/chrome',		//for pkg output linux
+            //executablePath: '/usr/bin/google-chrome'  //TODO add workaround to send video https://wwebjs.dev/guide/handling-attachments.html#caveat-for-sending-videos-and-gifs
+        }
+    });
+
+    client.initialize();
+
+    client.on('qr', (qr) => {
+        // NOTE: This event will not be fired if a session is specified.
+        qrcode.generate(qr, {small: true});
+    });
+
+    client.on('auth_failure', msg => {
+        // Fired if session restore was unsuccessful
+        console.error('WWEB AUTHENTICATION FAILURE', msg);
+    });
+
+
+    client.on('ready', () => {
+        console.log('WWEB READY');
+        client.sendPresenceAvailable();
+
+        try{
+            //const numbers = fs.readFileSync(path.resolve('numbers.txt'));
+            const numbers = fs.readFileSync(numbersFile);
+
+            var numbersArr = numbers.toString().split(",");
+            for(i in numbersArr) {
+                if(numbersArr[i]=='')
+                    continue;
+                var sanitized_number = numbersArr[i].toString().replace(/[- )(+]/g, "");	//numbers already with a prefix	//TODO add numbers without a prefix
+                var chatId=sanitized_number+'@c.us';	//'@c.us' represents a person's userdId
+
+                if(!client.isRegisteredUser(chatId)){
+                    console.timeLog(numbersArr[i]+' IS NOT ON WHATSAPP');
+                    continue;
+                }
+
+                //if message exists
+                if(messageToSend!='')
+                    client.sendMessage(chatId, messageToSend);
+                //if media exists
+                (mediaToSend).forEach(mediaPath => {
+                    client.sendMessage(chatId, MessageMedia.fromFilePath(mediaPath));
+                });
+                
+                console.log('SENDING MESSAGE TO '+numbersArr[i]);
+                
+                //TODO how much delay?
+                //delay to try avoiding ban
+                new Promise(resolve => setTimeout(resolve, randBetween(500,4500)))	//in ms
+            }
+            client.sendPresenceUnavailable();
+            console.log('ALL DONE!');
+
+        } catch(err){
+            console.error(err);
+        }
+    });
+}
+
+
+function randBetween(min, max) {  
+	return Math.floor(
+	  	Math.random() * (max - min + 1) + min
+	)
+}
---
+++ questions.js
@@ -0,0 +1,43 @@
+module.exports.ask = askInput;
+
+const inquirer = require('inquirer');
+const inquirerFileTreeSelection = require('inquirer-file-tree-selection-prompt');
+inquirer.registerPrompt('file-tree-selection', inquirerFileTreeSelection);
+
+//Ask message and media
+function askInput(callback){
+    inquirer
+        .prompt([
+            {
+                type: 'file-tree-selection',
+                name: 'numbers',
+                message: "Select the file with telephone numbers (They must be written with county code as a prefix and comma(,) separated) (Confirm with \'Enter\'):",	//Implement sending all media in a folder
+            },
+            {
+                type: 'input',
+                name: 'message',
+                message: "Insert the text you want to send (Then press Enter):",
+                validate(input) {
+                    if (/\S/g.test(input))
+                        return true;
+                    else{
+                        flagMessage=false;
+                        return true;
+                    }
+                },
+            },
+            {
+                type: 'file-tree-selection',
+                name: 'media',
+                message: "Select which files/media you want to send (Do not select entire folders) (Select with \'Space\' and confirm with \'Enter\'):",	//Implement sending all media in a folder
+                multiple: true,
+            },
+        ])
+        .then((answers) => {
+            /* numbersFile=answers.numbers;
+            messageToSend=answers.message;
+            mediaToSend=answers.media; */
+            callback(answers.numbers, answers.message, answers.media);
+        });
+        //TODO something to catch?
+}
