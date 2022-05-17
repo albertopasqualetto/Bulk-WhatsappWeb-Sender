28253a2b99416af12b180a8b07bea4efacfead24
Alberto Pasqualetto <39854348+albertopasqualetto@users.noreply.github.com>
2022-05-18 00:30:10 +0200

    Add option to use local Chromium anyway

--- index.js
+++ index.js
@@ -2,24 +2,50 @@ const questions=require('./questions');
 const WASend=require('./WASend');
 const fs=require('fs');
 const path=require('path');
+const { program } = require('commander');
 
 process.env.NODE_LAUNCH_MODE = 'native';	//'native'= "node index.js"; 'compiled'=index.exe
 global.pupPath='';
 
-//Check if chrome is installed
-fs.access(require('get-google-chrome-path').getGoogleChromePath(), fs.constants.F_OK, (err) => {
-	if(err){
-		if(process.env.NODE_LAUNCH_MODE === 'compiled'){
-			let dirPlatVer=fs.readdirSync(path.join(__dirname,'.local-chromium'));		//e.g.: ./.local-chromium
-			let dirPlat=fs.readdirSync(path.join(__dirname,'.local-chromium',dirPlatVer));	//e.g.: ./.local-chromium/linux-*
-			pupPath=path.join(__dirname,'.local-chromium',dirPlatVer,dirPlat,'chrome')	//e.g.: ./.local-chromium/linux-*/chrome-linux/chrome
-		}
-		//else pupPath=''
-	}
-	else{
-		pupPath=require('get-google-chrome-path').getGoogleChromePath();
-	}
-});
+program
+	.name('Bulk-WhatsappWeb-Sender')
+	.description('Send bulk messages to a telephone number list')
+	.version('1.1.1');
 
+program
+	/* .option('-n, --numbers')		//TODO use args options to bypass interaction
+	.option('-w, --write-message')
+	.option('-r, --read-message')
+	.option('-f, --files') */
+	.option('-l, --local-chromium', 'use local Chromium executable instead of intalled Chrome', false);
+
+program.parse();
+var options= program.opts();
+
+
+// TODO download chromium only if needed
+if(!options.localChromium){
+	//Check if chrome is installed
+	fs.access(require('get-google-chrome-path').getGoogleChromePath(), fs.constants.F_OK, (err) => {
+		if(err){
+			if(process.env.NODE_LAUNCH_MODE === 'compiled'){
+				let dirPlatVer=fs.readdirSync(path.join(__dirname,'.local-chromium'));		//e.g.: ./.local-chromium
+				let dirPlat=fs.readdirSync(path.join(__dirname,'.local-chromium',dirPlatVer));	//e.g.: ./.local-chromium/linux-*
+				pupPath=path.join(__dirname,'.local-chromium',dirPlatVer,dirPlat,'chrome')	//e.g.: ./.local-chromium/linux-*/chrome-linux/chrome
+			}
+			//else pupPath=''
+		}
+		else{
+			pupPath=require('get-google-chrome-path').getGoogleChromePath();
+		}
+	});
+}
+
+/* var numbersFile;
+var messageToSend;
+var mediaToSend; */
 
 questions.ask(WASend.send);		//Bootstrap
+
+/* await questions.ask();
+WASend.send(numbersFile, messageToSend, mediaToSend); */
